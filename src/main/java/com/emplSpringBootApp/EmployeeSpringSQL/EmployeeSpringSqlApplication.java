1695542d825a35b1a7322d9eb9421693fdae6986
HELBUSHEK <nemerov_a@mail.ru>
2024-02-05 01:01:24 +0800

    Initial commit

---
+++ src/main/java/com/emplSpringBootApp/EmployeeSpringSQL/EmployeeSpringSqlApplication.java
@@ -0,0 +1,13 @@
+package com.emplSpringBootApp.EmployeeSpringSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSpringSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emplSpringBootApp/EmployeeSpringSQL/EmployeeSpringSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emplSpringBootApp.EmployeeSpringSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSpringSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
